14b9884fdced24bb8bbb0350c09ffdf3d7cd0f74
RLynch <l00183513@atu.ie>
2026-01-29 12:44:56 +0000

    added a print saying goodbye and a message to the java code

--- HelloATU.java
+++ HelloATU.java
@@ -1,6 +1,8 @@
 public class HelloATU{
 public static void main(String[] args){
 //print hello, ATU from ryan as output
-System.Out.println("Hello, ATU from ryan");
+System.out.println("Hello, ATU from ryan");
+System.out.println("Goodbye, ATU from ryan");
 					}
 		       }
+// This is a simple Java program that prints "Hello, ATU from ryan" to the console.
